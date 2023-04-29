9d980550fa82a1e46c72e6b647fbd222e73cc0b8
yuriy <yuriy.faizov@gmail.com>
2023-04-29 22:32:33 +0300

    Change apod page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Counter from "./features/counter/Counter";
+import { ApodFull } from "./features/apod/apodFull";
 import { Apod } from "./features/apod/apod";
+import "./styles.scss";
+import ThemeIcon from "./theme.png";
 
 function App(): JSX.Element {
+  const savedDarkMode = JSON.parse(localStorage.getItem("darkMode") ?? "false");
+
+  // начальное значение состояния будет равно сохраненной теме из локального хранилища или
+  // настройкам устройства по умолчанию, если сохраненная тема не определена
+  const [darkMode, setDarkMode] = useState(
+    savedDarkMode ?? window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="App">
       {/* <Counter /> */}
-      <Apod />
+      {/* <ApodFull /> */}
+
+      <div className={darkMode ? "dark-mode" : ""}>
+        <button className="btnThemeMode" onClick={toggleDarkMode}>
+          <img src={ThemeIcon} alt="" width={30}/>
+        </button>
+        <Apod />
+      </div>
     </div>
   );
 }
--- src/features/apod/apod.tsx
+++ src/features/apod/apod.tsx
@@ -1,56 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import { useGetApodQuery } from "./apodApi";
+
 import LogoImg from "../../logo.png";
-import InfoImg from "../../info.png";
 
 import "./styles.scss";
 
 export const Apod = () => {
   const { data, error, isLoading } = useGetApodQuery("");
-  const [hidden, setHidden] = useState(true);
 
   if (error && !data) {
     return <div>error</div>;
   }
 
   if (isLoading) {
-    return <div>isLoading</div>;
+    return <></>;
   }
+
   const apod = data[0];
-  console.log("apod", apod);
+
   return (
-    <div
-      className="root"
-      style={{ backgroundImage: apod.hdurl && `url(${apod.hdurl})` }}
-    >
-      <div className="container">
+    <div className="root">
+      <div className="info">
         <div className="logo">
-          <img src={LogoImg} alt="logo" />
-        </div>
-        {!hidden && apod && (
-          <div className="info">
-            <h1>{apod.title}</h1>
-            <p>{apod.explanation}</p>
+          <img src={LogoImg} alt="" />
+          <div>
+            {" "}
+            <h3>Web NASA</h3>
+            <p>Astronomy Picture of the Day</p>
           </div>
+        </div>
+        <span className="date">{apod.date}</span>
+        <h1>{apod.title}</h1>
+        <p>{apod.explanation}</p>
+      </div>
+      <div className="media">
+        {/* <div className="full">
+          <a
+            href={apod.hdurl || apod.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          > 
+
+            <img src={LinkExtIcon} alt="" />
+          </a>
+        </div> */}
+        <a
+          href={apod.hdurl || apod.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={apod.hdurl} alt="" />
+        </a>
+        {apod.copyright && (
+          <div className="credit">Сopyright: {apod.copyright}</div>
         )}
       </div>
-      <div className="info-icon">
-        <img src={InfoImg} alt="" onClick={() => setHidden(!hidden)} />
-      </div>
-      {apod.media_type === "video" && (
-        <iframe
-          src={apod.url}
-          style={{
-            overflow: "hidden",
-            height: "100%",
-            width: "100%",
-            position: "absolute",
-            top: 0,
-          }}
-          height="100%"
-          width="100%"
-        ></iframe>
-      )}
     </div>
   );
 };
