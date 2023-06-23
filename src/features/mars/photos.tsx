ee71dbb91a20d8230b88130dea952be3ea4abe82
yuriy <yuriy.faizov@gmail.com>
2023-06-23 23:11:49 +0300

    Mars page, fix styles

--- src/features/mars/photos.tsx
+++ src/features/mars/photos.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useFetch, useReadLocalStorage } from "usehooks-ts";
 
-import { useFetch } from "usehooks-ts";
+import ImageNotFoundDark from "../../assets/images/404-rover-dark.svg";
+import ImageNotFoundLight from "../../assets/images/404-rover-light.svg";
 
-import "./styles.scss";
+import style from "./styles.module.scss";
 
 type Props = {
   date: Date;
@@ -21,6 +24,7 @@ type TPhoto = {
 };
 
 export const ListPhotos = ({ date, nameRover }: Props) => {
+  const darkMode = useReadLocalStorage("darkMode");
   const forrmattedDate = date.toISOString().split("T")[0];
 
   const { data, error } = useFetch<TPhotosArray>(
@@ -29,14 +33,26 @@ export const ListPhotos = ({ date, nameRover }: Props) => {
     }`
   );
 
+  if (data && data.photos.length === 0) {
+    return (
+      <div className={style.notFound}>
+        <h1>Sorry, no photos match</h1>
+        <p>Please try a different date or rover</p>
+        <img src={darkMode ? ImageNotFoundDark : ImageNotFoundLight} alt="" />
+      </div>
+    );
+  }
+
   return (
-    <div className="photos">
+    <div className={style.photos}>
       {data &&
         data.photos.length > 0 &&
         data.photos.map((item) => {
           return (
-            <div key={item.id} className="item">
-              <img src={item.img_src} alt="" loading="lazy" />
+            <div key={item.id} className={style.item}>
+              <Link to={item.img_src} target="_blank">
+                <img src={item.img_src} alt="" loading="lazy" />
+              </Link>
             </div>
           );
         })}
